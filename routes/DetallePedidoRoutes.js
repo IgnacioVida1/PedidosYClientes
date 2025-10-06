ea223ce9ac0d13a750d74f80eabc83abfeda998c
IgnacioVida1 <ignacio.vidal@utec.edu.pe>
2025-10-05 23:09:42 -0500

    Update DetallePedidoRoutes.js

--- routes/DetallePedidoRoutes.js
+++ routes/DetallePedidoRoutes.js
@@ -4,8 +4,8 @@ const DetalleController = require("../controllers/DetallePedidoController");
 
 router.post("/", DetalleController.agregarDetalle);
 router.put("/:id", DetalleController.actualizarDetalle);
-router.get("/:id", DetalleController.consultarPedidoId);
-router.get("/", DetalleController.consultarPedidos);
+router.get("/:id", DetalleController.consultarDetalleId);
+router.get("/", DetalleController.consultarDetalles);
 router.delete("/:id", DetalleController.borrarPedido);
 
 module.exports = router;
